0b2d72a3f040b7a71f695f9d32216b676b486cc4
Lars Verdoes <lars@MacBook-Air-van-Lars.local>
2021-07-03 10:27:08 +0200

    cleaned up a bit

--- src/list/ft_list_remove_one.c
+++ src/list/ft_list_remove_one.c
@@ -6,7 +6,7 @@
 /*   By: lverdoes <lverdoes@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/28 01:59:14 by lverdoes      #+#    #+#                 */
-/*   Updated: 2021/05/26 09:43:05 by lverdoes      ########   odam.nl         */
+/*   Updated: 2021/07/02 21:28:16 by lverdoes      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
 **	Remove a node from the list.
-**	The node is unlinked, freed and set to NULL.
+**	The node is unlinked and freed.
 **
 **	Ex.:
 **	ft_list_remove_one(&head, ft_list_find(head, "Rigate",  cmp_ref), del_var);
