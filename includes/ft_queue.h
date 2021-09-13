df91fdbceb438e4219b920766061e5570fd0a590
Lars Verdoes <lars@MacBook-Air-van-Lars.local>
2021-09-13 16:16:02 +0200

    .

--- includes/ft_queue.h
+++ includes/ft_queue.h
@@ -6,7 +6,7 @@
 /*   By: lverdoes <lverdoes@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/08/27 19:58:05 by lverdoes      #+#    #+#                 */
-/*   Updated: 2021/09/11 11:31:22 by lverdoes      ########   odam.nl         */
+/*   Updated: 2021/09/12 18:46:31 by lverdoes      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define FT_QUEUE_H
 
 # include <stddef.h>
-# include <stdio.h>
 
 typedef struct s_queue
 {
@@ -25,7 +24,7 @@ typedef struct s_queue
 	size_t	end;
 }	t_queue;
 
-void	ft_dequeue(t_queue *qq);
+void	ft_dequeue(t_queue *q);
 int		ft_enqueue(t_queue *q, void *data);
 void	ft_queue_clear(t_queue *q);
 void	*ft_queue_get_element(t_queue *q, size_t index);
